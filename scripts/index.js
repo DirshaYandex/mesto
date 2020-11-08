9899d2aea51d06bc59ebe305fbb0531ab87ddbc2
Alesya <alesya.dirsha@gmail.com>
2020-11-08 20:04:50 +0300

    Рефакторинг

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Переменные для попапа редактирования имени и профессии
 const popup = document.querySelector('.popup');
 const popupContent = document.querySelector('.popup__container');
 const popupTitle = document.querySelector('.popup__text');
@@ -12,37 +13,7 @@ const form = document.querySelector('.popup__form');
 const nameField = document.querySelector('.popup__field_type_name');
 const professionField = document.querySelector('.popup__field_type_title');
 
-
-function showPopup() {
-  popup.classList.add('popup_opened');
-  nameField.value = profile.textContent;
-  professionField.value = profession.textContent;
-}
-
-function closePopup() {
-  popup.classList.remove('popup_opened')
-}
-
-function popupClickHandler(event) {
-  if (event.target.classList.contains('popup')) {
-    closePopup()
-  }
-}
-
-function submitForm(event) {
-  event.preventDefault();
-  profile.textContent = nameField.value;
-  profession.textContent = professionField.value;
-  closePopup();
-}
-
-editButton.addEventListener('click', showPopup);
-popupCloseButton.addEventListener('click', closePopup);
-popup.addEventListener('mousedown', popupClickHandler)
-form.addEventListener('submit', submitForm);
-
-//еще больше js
-//Заполнение карточками
+//Список карточек
 const initialCards = [
   {
       name: 'Архыз',
@@ -69,21 +40,12 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
 ]; 
+
+//Переменные для template
 const pictureSection = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#pictures').content;
 
-function makePictureElement(item) {
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.pictures__title').textContent = item['name'];
-  pictureElement.querySelector('.pictures__img').src = item['link'];
-  pictureElement.querySelector('.pictures__img').alt = item['name'];
-  pictureSection.append(pictureElement);
-}
-
-initialCards.forEach(makePictureElement);
-
-// Попап на добавление карточки
-
+//Переменные для попапа на добавление новой карточки
 const addPopup = document.querySelector('.add-popup');
 const addPopupCloseButton = document.querySelector('.add-popup__close');
 const addButton = document.querySelector('.profile__add-button');
@@ -91,7 +53,81 @@ const addForm = document.querySelector('.add-popup__form');
 const imageNameField = document.querySelector('.add-popup__field_type_image-name');
 const imageLinkField = document.querySelector('.add-popup__field_type_image-link');
 
+//Переменные для попапа картинки
+const picturePopup = document.querySelector('.picture-popup')
+const picturePopupCloseButton = document.querySelector('.picture-popup__close');
+const picture = document.querySelector('.picture-popup__image');
+const pictureText = document.querySelector('.picture-popup__text');
 
+// попап редактирования имени и профессии
+function showPopup() {
+  popup.classList.add('popup_opened');
+  nameField.value = profile.textContent;
+  professionField.value = profession.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened')
+}
+
+function popupClickHandler(event) {
+  if (event.target.classList.contains('popup')) {
+    closePopup()
+  }
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  profile.textContent = nameField.value;
+  profession.textContent = professionField.value;
+  closePopup();
+}
+
+//Попап картинки
+function showPicturePopup() {
+  picturePopup.classList.add('picture-popup_opened');
+}
+
+function closePicturePopup() {
+  picturePopup.classList.remove('picture-popup_opened')
+}
+
+function picturePopupClickHandler(event) {
+  if (event.target.classList.contains('picture-popup')) {
+    closePicturePopup()
+  }
+}
+
+//Создание карточки и обработка лайка, кнопки удаления, попапа картинки
+function createPictureElement(name, link) {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.pictures__title').textContent = name;
+  pictureElement.querySelector('.pictures__img').src = link;
+  pictureElement.querySelector('.pictures__img').alt = name;
+  pictureElement.querySelector('.pictures__like').addEventListener('click', event => {
+    event.currentTarget.classList.toggle('pictures__like_black');
+  });
+  pictureElement.querySelector('.pictures__img-delete').addEventListener('click', event => {
+    event.target.parentElement.remove();
+  });
+  pictureElement.querySelector('.pictures__img').addEventListener('click', event => {
+    showPicturePopup();
+    picture.src = event.target.src
+    picture.alt = event.target.alt
+    pictureText.textContent = event.target.alt
+  });
+  return pictureElement;
+}
+
+// инициализация карточек при загрузке страницы
+function initPictureElement(item) {
+  const pictureElement = createPictureElement(item['name'],item['link']);
+  pictureSection.append(pictureElement);
+}
+
+initialCards.forEach(initPictureElement);
+
+//Попап добавления карточки
 function showAddPopup() {
   addPopup.classList.add('popup_opened');
   imageNameField.value = '';
@@ -110,90 +146,25 @@ function addPopupClickHandler(event) {
 
 function addSubmitForm(event) {
   event.preventDefault();
-  const pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.pictures__title').textContent = imageNameField.value;
-  pictureElement.querySelector('.pictures__img').src  = imageLinkField.value;
-  pictureElement.querySelector('.pictures__img').alt = imageNameField.value;
+  const pictureElement = createPictureElement(imageNameField.value, imageLinkField.value);
   pictureSection.prepend(pictureElement);
   closeAddPopup();
   selectAllLikes();
   selectDeleteButtons();
 }
 
+// кнопки обработки попапа редактирования имени и профессии
+editButton.addEventListener('click', showPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popup.addEventListener('mousedown', popupClickHandler)
+form.addEventListener('submit', submitForm);
+
+// кнопки обработки попапа добавления новой карточки
 addButton.addEventListener('click', showAddPopup);
 addPopupCloseButton.addEventListener('click', closeAddPopup);
 addPopup.addEventListener('mousedown', addPopupClickHandler)
 addForm.addEventListener('submit', addSubmitForm);
 
-//Лайк
-function likeClickHandler(event) {
-  event.currentTarget.classList.toggle('pictures__like_black');
-}
-
-function makeLike(item) {
-  item.addEventListener('click', likeClickHandler);
-}
-
-function selectAllLikes() {
-  document.querySelectorAll('.pictures__like').forEach(makeLike);
-}
-
-selectAllLikes();
-
-// Удаление карточки
-
-function deleteCard(event) {
-  event.target.parentElement.remove(); 
-}
-
-function makeDelete(item) {
-  item.addEventListener('click', deleteCard);
-}
-
-function selectDeleteButtons() {
-  document.querySelectorAll('.pictures__img-delete').forEach(makeDelete);
-}
-
-selectDeleteButtons();
-
-//Попап картинки
-
-const picturePopup = document.querySelector('.picture-popup')
-const picturePopupCloseButton = document.querySelector('.picture-popup__close');
-const picture = document.querySelector('.picture-popup__image');
-const pictureText = document.querySelector('.picture-popup__text');
-
-function showPicturePopup() {
-  picturePopup.classList.add('picture-popup_opened');
-
-}
-
-function closePicturePopup() {
-  picturePopup.classList.remove('picture-popup_opened')
-}
-
-function picturePopupClickHandler(event) {
-  if (event.target.classList.contains('picture-popup')) {
-    closePicturePopup()
-  }
-}
-
-function increasePicture(event) {
-  showPicturePopup();
-  picture.src = event.target.src
-  picture.alt = event.target.alt
-  pictureText.textContent = event.target.alt
-}
-
-function pictureClickHandler(item) {
-  item.addEventListener('click', increasePicture);
-  console.log('Нажали на картиночку')
-}
-
-function selectAllPictures() {
-  document.querySelectorAll('.pictures__img').forEach(pictureClickHandler);
-}
-
-selectAllPictures();
+// кнопки обработки попапа картинки
 picturePopupCloseButton.addEventListener('click', closePicturePopup);
-picturePopup.addEventListener('mousedown', picturePopupClickHandler)
+picturePopup.addEventListener('mousedown', picturePopupClickHandler);
