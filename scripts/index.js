2bbad47ca8e9df50645a03aa8c2c84072b339cb8
Alesya <alesya.dirsha@gmail.com>
2020-11-08 17:00:51 +0300

    feat: добавление и удаление карточки на странице

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,6 @@ const popupTitle = document.querySelector('.popup__text');
 
 const popupCloseButton = document.querySelector('.popup__close');
 const editButton = document.querySelector('.profile__edit-button');
-const addButton = document.querySelector('.profile__add-button');
 
 const profile = document.querySelector('.profile__name');
 const profession = document.querySelector('.profile__profession');
@@ -83,10 +82,79 @@ function makePictureElement(item) {
 
 initialCards.forEach(makePictureElement);
 
-//Лайк
+// Попап на добавление карточки
 
-document.querySelectorAll('.pictures__like').forEach(item => {
-  item.addEventListener('click', event => {
-    event.currentTarget.classList.toggle('pictures__like_black');
-  })
-});
+const addPopup = document.querySelector('.add-popup');
+const addPopupCloseButton = document.querySelector('.add-popup__close');
+const addButton = document.querySelector('.profile__add-button');
+const addForm = document.querySelector('.add-popup__form');
+const imageNameField = document.querySelector('.add-popup__field_type_image-name');
+const imageLinkField = document.querySelector('.add-popup__field_type_image-link');
+
+
+function showAddPopup() {
+  addPopup.classList.add('popup_opened');
+  imageNameField.value = '';
+  imageLinkField.value = '';
+}
+
+function closeAddPopup() {
+  addPopup.classList.remove('popup_opened')
+}
+
+function addPopupClickHandler(event) {
+  if (event.target.classList.contains('add-popup')) {
+    closeAddPopup()
+  }
+}
+
+function addSubmitForm(event) {
+  event.preventDefault();
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.pictures__title').textContent = imageNameField.value;
+  pictureElement.querySelector('.pictures__img').src  = imageLinkField.value;
+  pictureElement.querySelector('.pictures__img').alt = imageNameField.value;
+  pictureSection.prepend(pictureElement);
+  closeAddPopup();
+  selectAllLikes();
+  selectDeleteButtons();
+}
+
+addButton.addEventListener('click', showAddPopup);
+addPopupCloseButton.addEventListener('click', closeAddPopup);
+addPopup.addEventListener('mousedown', addPopupClickHandler)
+addForm.addEventListener('submit', addSubmitForm);
+
+//Лайк
+function likeClickHandler(event) {
+  event.currentTarget.classList.toggle('pictures__like_black');
+}
+
+function makeLike(item) {
+  item.addEventListener('click', likeClickHandler);
+}
+
+function selectAllLikes() {
+  document.querySelectorAll('.pictures__like').forEach(makeLike);
+}
+
+selectAllLikes();
+
+// Удаление карточки
+
+function deleteCard(event) {
+  console.log(event.target.parentElement)
+  event.target.parentElement.remove();
+  
+}
+
+function makeDelete(item) {
+  item.addEventListener('click', deleteCard);
+  console.log('fff')
+}
+
+function selectDeleteButtons() {
+  document.querySelectorAll('.pictures__img-delete').forEach(makeDelete);
+}
+
+selectDeleteButtons();
